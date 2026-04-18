202a1c1195125a3a75db79f1250d6d6fdb80be13
rhuanlucasdev <rhuanlucassbs@gmail.com>
2026-04-18 15:19:01 -0300

    feat: setup fullstack CRUD with NestJS, Prisma, PostgreSQL and React + MUI

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
+  }
+
+  findAll(name?: string) {
+    return this.prisma.user.findMany({
+      where: {
+        name: name
+          ? {
+              contains: name,
+              mode: 'insensitive',
+            }
+          : undefined,
+      },
+    });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
